f3489326697fcfc72f0fbcab860cd15a7965fa85
larezafw <farhanlareza@gmail.com>
2023-01-08 10:41:18 +0700

    Init

---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,46 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/lareza-farhan-wanaghi/bookings/pkg/config"
+	"github.com/lareza-farhan-wanaghi/bookings/pkg/models"
+	"github.com/lareza-farhan-wanaghi/bookings/pkg/render"
+)
+
+// Repository is the repository type
+type Repository struct {
+	App *config.AppConfig
+}
+
+// Repo the repository used by the handlers
+var Repo *Repository
+
+// NewHandlers sets the repository for the handlers
+func NewHandlers(a *config.AppConfig) {
+	Repo = &Repository{
+		App: a,
+	}
+}
+
+// Home is the handler for the home page
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
+}
+
+// About is the handler for the about page
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+	// perform some logic
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello, again"
+
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIP
+
+	// send data to the template
+	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
+		StringMap: stringMap,
+	})
+}
